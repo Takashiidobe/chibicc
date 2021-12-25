2bc5eb2dfa5997701f9db4a3e469ac409a249c39
takashiidobe <idobetakashi@gmail.com>
2021-12-25 12:30:55 -0500

    add `+` and `-` operations

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 while true; do
-  sleep 2s;
-  rsync -azP --prune-empty-dirs --include="*/" --include="*.sh" --include="*.c" --exclude="*" ~/Desktop/chibicc "kflqez71@45.79.210.61:/home/kflqez71/"
+  sleep 5s;
+  find . | entr -d rsync -azP --prune-empty-dirs --include="*/" --include="*.sh" --include="*.c" --exclude="*" ~/Desktop/chibicc "kflqez71@45.79.210.61:/home/kflqez71/"
 done
